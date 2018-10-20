8f12990b08d8e5b1ca62b0bd8798d0023ea903a8
h4nds0m3j4ck <estrella.terenzio@gmail.com>
2018-10-20 11:31:47 -0400

    updated UserProInfo.js

--- models/UserProInfo.js
+++ models/UserProInfo.js
@@ -3,11 +3,35 @@ const Schema = mongoose.Schema;
 
 const UserProInfoSchema = new Schema({
   //Atributes for UserProInfo
+  
+  id_firebase: {
+    type: String,
+    trim: true,
+    required: "Username is Required"
+  },
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [
+      function(input) {
+        return input.length >= 6;
+      },
+      "Password should be longer."
+    ]
+  },
   phone: Number,
   social: Array,
-  id_firebase: Schema.Types.Mixed,
-  email: String,
-  expertise: Array
+  expertise: Array,
+  userCreated: {
+    type: Date,
+    default: Date.now
+  },
 });
 
 const UserProInfo = mongoose.model("UserProInfo", UserProInfoSchema);
